a0e48d074742ff8fb8569190c8c41615078e5e68
Juan Ammiraglia <jammiraglia@metalfor.local>
2025-07-03 12:10:54 -0300

    corriguiendo ruta

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -2,9 +2,10 @@ import axios from 'axios';
 import { Task, ProcessExcelResponse } from '../types/task';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api/v1',
+  baseURL: 'https://pruebadeproduccion-production.up.railway.app/api/v1',
   timeout: 10000,
 });
+
 
 api.interceptors.request.use(
   (config) => {
