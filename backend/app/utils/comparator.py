25163067aeec5c8496cc41c0432062bf36a68b1c
Juan Ammiraglia <jammiraglia@metalfor.local>
2025-07-08 15:35:57 -0300

    manejo de espacios vacios

--- backend/app/utils/comparator.py
+++ backend/app/utils/comparator.py
@@ -4,9 +4,6 @@ import pandas as pd
 
 def normalize_value(value):
     # Convertir NaN (float), NAType y string "nan" explícito a None
-    if value is None:
-        return None
-
     if isinstance(value, float) and math.isnan(value):
         return None
 
@@ -32,3 +29,12 @@ def normalize_value(value):
         return value.date().isoformat()
 
     return value
+
+def compare_objects(obj1: dict, obj2: dict) -> list[str]:
+    diffs = []
+    for key in obj1.keys():
+        val1 = normalize_value(obj1.get(key))
+        val2 = normalize_value(obj2.get(key))
+        if val1 != val2:
+            diffs.append(f"[DIFF] {key}: {val1} != {val2}")
+    return diffs
