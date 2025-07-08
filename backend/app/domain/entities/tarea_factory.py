a0449f8934e534f4d3c9ac29165ca0b4cd44d557
Juan Ammiraglia <jammiraglia@metalfor.local>
2025-07-08 12:45:58 -0300

    manejo de espacios vacios

--- backend/app/domain/entities/tarea_factory.py
+++ backend/app/domain/entities/tarea_factory.py
@@ -1,25 +1,26 @@
 from .tarea import Tarea
+from app.utils.comparator import normalize_value  # Asegurate de importar correctamente
 
 class TareaFactory:
     @staticmethod
     def crear_desde_dict(r: dict) -> Tarea:
         return Tarea(
-            id_de_tarea=r.get("Id. de tarea"),
-            nombre_de_la_tarea=r.get("Nombre de la tarea"),
-            nombre_del_deposito=r.get("Nombre del depósito"),
-            progreso=r.get("Progreso"),
-            priority=r.get("Priority"),
-            asignado_a=r.get("Asignado a"),
-            creado_por=r.get("Creado por"),
-            fecha_de_creacion=r.get("Fecha de creación"),
-            fecha_de_inicio=r.get("Fecha de inicio"),
-            fecha_de_vencimiento=r.get("Fecha de vencimiento"),
-            fecha_de_finalizacion=r.get("Fecha de finalización"),
-            es_periodica=r.get("Es periódica"),
-            con_retraso=r.get("Con retraso"),
-            completado_por=r.get("Completado por"),
-            checklist_completados=r.get("Elementos de la lista de comprobación completados"),
-            checklist_total=r.get("Elementos de la lista de comprobación"),
-            etiquetas=r.get("Etiquetas"),
-            descripcion=r.get("Descripción")
+            id_de_tarea=normalize_value(r.get("Id. de tarea")),
+            nombre_de_la_tarea=normalize_value(r.get("Nombre de la tarea")),
+            nombre_del_deposito=normalize_value(r.get("Nombre del depósito")),
+            progreso=normalize_value(r.get("Progreso")),
+            priority=normalize_value(r.get("Priority")),
+            asignado_a=normalize_value(r.get("Asignado a")),
+            creado_por=normalize_value(r.get("Creado por")),
+            fecha_de_creacion=normalize_value(r.get("Fecha de creación")),
+            fecha_de_inicio=normalize_value(r.get("Fecha de inicio")),
+            fecha_de_vencimiento=normalize_value(r.get("Fecha de vencimiento")),
+            fecha_de_finalizacion=normalize_value(r.get("Fecha de finalización")),
+            es_periodica=normalize_value(r.get("Es periódica")),
+            con_retraso=normalize_value(r.get("Con retraso")),
+            completado_por=normalize_value(r.get("Completado por")),
+            checklist_completados=normalize_value(r.get("Elementos de la lista de comprobación completados")),
+            checklist_total=normalize_value(r.get("Elementos de la lista de comprobación")),
+            etiquetas=normalize_value(r.get("Etiquetas")),
+            descripcion=normalize_value(r.get("Descripción")),
         )
